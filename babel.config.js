8ca6ad86abe1d2ac7af10969b5be8d6ebc39f219
Francis Li <mail@francisli.com>
2023-07-28 16:04:20 -0700

    Configure babel for react-native-dotenv, upgrade prettier, reformat code with prettier

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,4 @@
 module.exports = {
+  plugins: [['module:react-native-dotenv']],
   presets: ['module:metro-react-native-babel-preset'],
 };
--- screens/main/home.js
+++ screens/main/home.js
@@ -117,8 +117,7 @@ export default function HomeScreen({navigation, route}) {
       }
       setTodaysWalk(newTodaysWalk);
     }
-  },
-  []);
+  }, []);
 
   async function getTotalSteps() {
     setTotalSteps(null);
