70a7213c6fcf8a354c7221929f55f69a8de6d44b
Francis Li <mail@francisli.com>
2023-11-24 15:57:35 -0800

    Set up React Native Vector Icons for iOS, Material icons only

---
+++ react-native.config.js
@@ -0,0 +1,10 @@
+// https://www.npmjs.com/package/react-native-vector-icons#ios-setup
+module.exports = {
+  dependencies: {
+    'react-native-vector-icons': {
+      platforms: {
+        ios: null,
+      },
+    },
+  },
+};
