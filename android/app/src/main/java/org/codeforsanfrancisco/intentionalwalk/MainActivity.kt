f927f8fbe5d1087240cb2a8376b39a453ba90a97
Francis Li <mail@francisli.com>
2024-01-11 20:27:15 -0800

    Fixes to get running on Android

--- android/app/src/main/java/org/codeforsanfrancisco/intentionalwalk/MainActivity.kt
+++ android/app/src/main/java/org/codeforsanfrancisco/intentionalwalk/MainActivity.kt
@@ -1,4 +1,4 @@
-package org.codeforsanfrancisco.intentionalwalk;
+package org.codeforsanfrancisco.intentionalwalk
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/org/codeforsanfrancisco/intentionalwalk/MainApplication.kt
+++ android/app/src/main/java/org/codeforsanfrancisco/intentionalwalk/MainApplication.kt
@@ -1,4 +1,4 @@
-package org.codeforsanfrancisco.intentionalwalk;
+package org.codeforsanfrancisco.intentionalwalk
 
 import android.app.Application
 import com.facebook.react.PackageList
