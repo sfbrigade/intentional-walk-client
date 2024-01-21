e33e0af6c1d9bd5bd6747ed5365c654c558711fb
Francis Li <mail@francisli.com>
2024-01-21 13:50:56 -0800

    Restore/configure modules for react-navigation

--- android/app/src/main/java/org/codeforsanfrancisco/intentionalwalk/MainActivity.kt
+++ android/app/src/main/java/org/codeforsanfrancisco/intentionalwalk/MainActivity.kt
@@ -1,11 +1,19 @@
 package org.codeforsanfrancisco.intentionalwalk
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
+  /**
+   * onCreate override added as per React Navigation installation guide.
+   * https://reactnavigation.org/docs/getting-started/
+   */
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
--- index.js
+++ index.js
@@ -2,6 +2,10 @@
  * @format
  */
 
+// react-native-gesture-handler imported as a peer dependency
+// of @react-navigation/stack, see documentation:
+// https://reactnavigation.org/docs/stack-navigator
+import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
