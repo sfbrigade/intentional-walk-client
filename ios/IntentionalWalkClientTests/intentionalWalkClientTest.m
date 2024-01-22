9c0e2ae2db6bbb2947adb9440f96152ee8493e15
Francis Li <mail@francisli.com>
2024-01-21 16:41:19 -0800

    [Closes #36] Rename intentionaWalkClient to IntentionalWalkClient

--- ios/IntentionalWalkClientTests/intentionalWalkClientTest.m
+++ ios/IntentionalWalkClientTests/intentionalWalkClientTest.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface intentionalWalkClientTests : XCTestCase
+@interface IntentionalWalkClientTests : XCTestCase
 
 @end
 
-@implementation intentionalWalkClientTests
+@implementation IntentionalWalkClientTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
